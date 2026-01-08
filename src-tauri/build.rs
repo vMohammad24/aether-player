14eeca5eeff8a96a7707f84609e5960ec0320406
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-01-09 00:58:25 +0300

    fix(build): fix windows mpv link path

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,12 @@
 fn main() {
     #[cfg(target_os = "windows")]
     {
-        let appdata_dir = std::env::var("APPDATA").unwrap();
-        let path = std::path::Path::new(&appdata_dir).join("mpv").join("lib");
+        let path = if let Ok(mpv_source) = std::env::var("MPV_SOURCE") {
+            std::path::PathBuf::from(mpv_source)
+        } else {
+            let appdata_dir = std::env::var("APPDATA").unwrap();
+            std::path::Path::new(&appdata_dir).join("mpv").join("lib")
+        };
 
         println!("cargo:rustc-link-search=native={}", path.display());
         println!("cargo:rustc-link-lib=static=mpv");
